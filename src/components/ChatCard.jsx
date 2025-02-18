6b55892bc901ecd6a52de6973b1b4359fe8e07d9
arjun r <arjun.rdell@gmail.com>
2025-02-18 20:04:28 +0530

    created chatCard reusable component

---
+++ src/components/ChatCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '../images/chatAppLogo.png';
+
+export default function ChatCard({name ,lastMessage}) {
+    return (
+        <div className='themeCard flex flex-row items-center w-4/5 rounded-md py-2 px-1.5 mb-2 cursor-pointer'>
+            <div className='themeCard w-fit rounded-full me-2'>
+                <img className='w-10' src={Logo} alt="userIcon" />
+            </div>
+            <div>
+                <h1>{name}</h1>
+                <p className='text-xs'>{lastMessage}</p>
+            </div>
+        </div>
+    )
+}
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../App.css';
-import Logo from '../images/chatAppLogo.png';
+import Logo from "../images/chatAppLogo.png";
+import ChatCard from '../components/ChatCard';
+import ChatSectionBg from "../images/loginPageBg-image.png";
 
 export default function Chat() {
   return (
@@ -10,18 +12,16 @@ export default function Chat() {
           <img className='w-10 me-2' src={Logo} alt="logo" />
           <h1 className='text-lg font-medium'>JellyFish</h1>
         </div>
-        <div className='themeCard flex flex-row items-center w-4/5 rounded-md py-2 px-1.5'>
-          <div className='themeCard w-fit rounded-full me-2'>
-            <img className='w-10' src={Logo} alt="userIcon" />
-          </div>
-          <div>
-            <h1>Name</h1>
-            <p className='text-xs'>Last Meaasage</p>
-          </div>
-        </div>
+        <ChatCard name="Rohit" lastMessage="Hi..." />
+        <ChatCard name="Rohit" lastMessage="Hi..." />
+        <ChatCard name="Rohit" lastMessage="Hi..." />
+        <ChatCard name="Rohit" lastMessage="Hi..." />
       </div>
       <div className='themeCard h-full m-2 rounded-md' style={{ width: "75%" }}>
-        right
+        <h1 className='text-center text-lg font-medium py-3'>Name</h1>
+        <div className='flex justify-center items-center'>
+          <img className='my-auto' src={ChatSectionBg} alt="img" />
+        </div>
       </div>
     </div>
   )
