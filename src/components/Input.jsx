d89ccd6d03026def5bb4ea53e279a86ba458757a
arjun r <arjun.rdell@gmail.com>
2025-02-19 22:24:35 +0530

    autocomplete is on only when name is Message

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,9 +3,11 @@ import '../App.css'
 
 export default function Input({ name, type }) {
     return (
-        <div className='relative w-full' style={name === "Message"?{marginBottom:"0"}:{marginBottom:"1rem"}}>
-            <input id='inputText' className='inputForm rounded-md p-2' placeholder={name === "Message" ? "Message" : ""} 
-            style={name === "Message" ? { border: "1px solid #1515154d",width:"100%" }:{ border: "1px solid #1515154d",width:"18rem" }} type={type} />
+        <div className='relative w-full' style={name === "Message" ? { marginBottom: "0" } : { marginBottom: "1rem" }}>
+            <input id='inputText' className='inputForm rounded-md p-2' placeholder={name === "Message" ? "Message" : ""}
+                style={name === "Message" ? { border: "1px solid #1515154d", width: "100%" } : { border: "1px solid #1515154d", width: "18rem" }}
+                type={type}
+                autocomplete={name === "Message"?"off":"on"} />
             {name === "Message" ? <></> : <label className='inputLabel' htmlFor="inputText">{name}</label>}
         </div>
     )
