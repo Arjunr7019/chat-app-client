75425c61460bd46a08f7fd98b0caff28992dfa42
arjun r <arjun.rdell@gmail.com>
2025-02-21 22:11:30 +0530

    setting userData in UserAuthContext

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import '../App.css';
 import { Link } from 'react-router-dom';
 import Input from '../components/Input';
 import Button from '../components/Button';
+import { UserAuthContext } from '../context/UserAuth';
 
 export default function Login() {
-  const[inputData,setInputData] = useState({email:"",password:""});
+  const [inputData, setInputData] = useState({ email: "", password: "" });
+  const { setUserData } = useContext(UserAuthContext);
 
   const handelLogin = () => {
     fetch("https://chat-app-server-0lgm.onrender.com/api/user/login", {
@@ -18,7 +20,7 @@ export default function Login() {
       }),
     })
       .then((response) => response.json())
-      .then((data) => console.log("Success:", data))
+      .then((data) => setUserData(data))
       .catch((error) => console.error("Error:", error));
   };
 
@@ -26,14 +28,14 @@ export default function Login() {
     <div className='flex justify-center items-center h-screen'>
       <div className='themeCard px-5 rounded-md py-5'>
         <h1 className='text-center text-3xl mb-4'>Login</h1>
-        <Input value={inputData.email} onChange={(e)=> setInputData(val=>{return{...val, email:e.target.value}})} name="Email" type="Text" />
-        <Input value={inputData.password} onChange={(e)=> setInputData(val=>{return{...val, password:e.target.value}})} name="Password" type="Password" />
+        <Input value={inputData.email} onChange={(e) => setInputData(val => { return { ...val, email: e.target.value } })} name="Email" type="Text" />
+        <Input value={inputData.password} onChange={(e) => setInputData(val => { return { ...val, password: e.target.value } })} name="Password" type="Password" />
         <div className='flex justify-center items-center mb-1'>
           <Link className='text-center cursor-pointer w-full text-sm' to="/register">Create an Account</Link>
         </div>
         <p className='text-center mb-3 cursor-pointer text-sm'>Forgot Password</p>
         <div className='flex justify-center items-center mb-3'>
-          <Button onClick={()=> handelLogin()} name="Login"/>
+          <Button onClick={() => handelLogin()} name="Login" />
         </div>
       </div>
     </div>
