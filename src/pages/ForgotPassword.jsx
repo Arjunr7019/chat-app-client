c178912182c0a31e8355663dd66c82ae474e8c2b
arjun r <arjun.rdell@gmail.com>
2025-04-02 22:57:10 +0530

    forgotPassword get OTP section added

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import '../App.css';
+import { Link } from 'react-router-dom';
 
 export default function ForgotPassword() {
-  return (
-    <div>
-      forgot password
-    </div>
-  )
+    const [inputData, setInputData] = useState({ email: "", otp: "" });
+
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <div className='themeCard px-5 rounded-md py-5'>
+                <h1 className='text-center text-3xl mb-4'>Login</h1>
+                <div className='flex flex-row w-full'>
+                    <Input
+                        value={inputData.email}
+                        onChange={(e) => setInputData(val => { return { ...val, email: e.target.value } })}
+                        name="Email"
+                        type="Text" />
+                        <Button name="Get OTP"/>
+                </div>
+                <div className='flex justify-center items-center mb-3'>
+                    <Link className='text-center cursor-pointer w-full text-sm' to="/login">Back to Login</Link>
+                </div>
+                {/* <div className='flex justify-center items-center mb-3'>
+          <Button name="Login" />
+        </div> */}
+            </div>
+        </div>
+    )
 }
