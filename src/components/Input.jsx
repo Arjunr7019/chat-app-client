8fa0cb5b4c011a77daa814f25fa85655fd3998e3
arjun r <arjun.rdell@gmail.com>
2025-04-03 22:25:24 +0530

    otp and new password section added to forgot password page and when otp is generated this two input section will show

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,6 +5,7 @@ import { IoEyeOffOutline } from "react-icons/io5";
 
 export default function Input({ name, type,value,onChange }) {
     const [showPassword,setShowPassword] = useState(false);
+    const nameCheck = ["Message","Password","OTP","New Password"]
     return (
         <div className='relative w-full' style={name === "Message" ? { marginBottom: "0" } : { marginBottom: "1rem" }}>
             <input
@@ -13,7 +14,7 @@ export default function Input({ name, type,value,onChange }) {
                 placeholder={name === "Message" ? "Message" : ""}
                 style={name === "Message" ? { width: "100%" } : { width: "18rem" }}
                 type={showPassword ? "text": type}
-                autoComplete={name === "Message" ? "off" : "on"} 
+                autoComplete={nameCheck.includes(name) ? "off" : "on"} 
                 onChange={onChange}
                 value={value}/>
             {name === "Message" ? <></> : <label className={value===""?'inputLabel':'inputLabelActive'} htmlFor="inputText">{name}</label>}
--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -5,26 +5,39 @@ import '../App.css';
 import { Link } from 'react-router-dom';
 
 export default function ForgotPassword() {
-    const [inputData, setInputData] = useState({ email: "", otp: "" });
+    const [inputData, setInputData] = useState({ email: "", otp: "",password:"" });
+    const [otpGenerated, setOtpGenerated] = useState(false);
 
     return (
         <div className='flex justify-center items-center h-screen'>
             <div className='themeCard px-5 rounded-md py-5'>
-                <h1 className='text-center text-3xl mb-4'>Login</h1>
+                <h1 className='text-center text-2xl mb-4'>Forgot Password</h1>
                 <div className='flex flex-row w-full'>
                     <Input
                         value={inputData.email}
                         onChange={(e) => setInputData(val => { return { ...val, email: e.target.value } })}
                         name="Email"
                         type="Text" />
-                        <Button name="Get OTP"/>
+                    <Button name="Get OTP" onClick={() => setOtpGenerated(true)} />
                 </div>
+                {otpGenerated ? <div>
+                    <Input
+                        value={inputData.email}
+                        onChange={(e) => setInputData(val => { return { ...val, otp: e.target.value } })}
+                        name="OTP"
+                        type="Text" />
+                    <Input
+                        value={inputData.email}
+                        onChange={(e) => setInputData(val => { return { ...val, password: e.target.value } })}
+                        name="New Password"
+                        type="Text" />
+                </div> : <></>}
                 <div className='flex justify-center items-center mb-3'>
                     <Link className='text-center cursor-pointer w-full text-sm' to="/login">Back to Login</Link>
                 </div>
-                {/* <div className='flex justify-center items-center mb-3'>
-          <Button name="Login" />
-        </div> */}
+                {otpGenerated ? <div className='flex justify-center items-center mb-3'>
+                    <Button name="Submit" />
+                </div>:<></>}
             </div>
         </div>
     )
