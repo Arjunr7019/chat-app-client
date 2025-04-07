ad8dc00393c1e12cc1ea4a9de4edddc448cb790a
arjun r <arjun.rdell@gmail.com>
2025-04-07 23:13:42 +0530

    some minor changes in button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Button({ name,onClick,extraClassNames }) {
-  const nameCheck = ["Send","Find","Add New User","Back To Chats","Add","Get OTP"]
+export default function Button({ name, onClick, extraClassNames }) {
+  const nameCheck = [ "Send","Find", "Add New User", "Back To Chats", "Add"]
   return (
     <button
-    onClick={onClick}
+      onClick={onClick}
       className={`px-2 py-1 rounded-md cursor-pointer buttonTheme ${extraClassNames}`}
       style={nameCheck.includes(name) ? { border: "1px solid #1515154d", height: "100%" } : { border: "1px solid #1515154d" }}
     >{name}</button>
