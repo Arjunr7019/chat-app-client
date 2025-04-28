1f70fec328329e4ab75c8da5a6e431eb0b21ed7e
arjun r <arjun.rdell@gmail.com>
2025-04-28 23:14:13 +0530

    otp resend timer added

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import '../App.css';
@@ -7,12 +7,25 @@ import { UserAuthContext } from '../context/UserAuth';
 import { Toaster, toast } from 'sonner'
 
 export default function ForgotPassword() {
-    const [inputData, setInputData] = useState({ email: "", otp: "",password:"" });
-    const { otpSendSuccessfully,getOtp } = useContext(UserAuthContext);
+    const [inputData, setInputData] = useState({ email: "", otp: "", password: "" });
+    const { otpSendSuccessfully, getOtp } = useContext(UserAuthContext);
+    const [secondsLeft, setSecondsLeft] = useState(60);
+
+    useEffect(() => {
+        let timer;
+
+        if (otpSendSuccessfully && secondsLeft > 0) {
+            timer = setInterval(() => {
+                setSecondsLeft(prev => prev - 1);
+            }, 1000);
+        }
+
+        return () => clearInterval(timer);
+    }, [secondsLeft,otpSendSuccessfully])
 
     return (
         <div className='flex justify-center items-center h-screen'>
-            <Toaster position="top-center"/>
+            <Toaster position="top-center" />
             <div className='themeCard px-5 rounded-md py-5'>
                 <h1 className='text-center text-2xl mb-4'>Forgot Password</h1>
                 <div className='flex flex-row w-full'>
@@ -21,8 +34,8 @@ export default function ForgotPassword() {
                         onChange={(e) => setInputData(val => { return { ...val, email: e.target.value } })}
                         name="Email"
                         type="Text" />
-                    <Button name="Get OTP" extraClassNames="w-1/4 mb-4 ms-2" 
-                    onClick={() => {getOtp(inputData.email); inputData.email ==="" ? toast.warning('Email input is empty') : ""} } />
+                    <Button name={otpSendSuccessfully ? secondsLeft === 0?"Resend":`${secondsLeft}` : "Get OTP"} extraClassNames="w-1/4 mb-4 ms-2"
+                        onClick={() => { getOtp(inputData.email); inputData.email === "" ? toast.warning('Email input is empty') : "" }} />
                 </div>
                 {otpSendSuccessfully ? <div>
                     <Input
@@ -41,7 +54,7 @@ export default function ForgotPassword() {
                 </div>
                 {otpSendSuccessfully ? <div className='flex justify-center items-center mb-3'>
                     <Button name="Submit" />
-                </div>:<></>}
+                </div> : <></>}
             </div>
         </div>
     )
