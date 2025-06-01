0910e8eda5192351c9784b9dc0ca652813f53d99
arjun r <arjun.rdell@gmail.com>
2025-06-01 19:58:23 +0530

    activeChatList is added to mobile screen

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -25,6 +25,7 @@ export default function Chat() {
   const [textMessage, setTextMessage] = useState();
   const [email, setEmail] = useState();
   const [findNewUser, setFindNewUser] = useState(false);
+  const [mobileActiveChat, setMobileActiveChat] = useState(true);
 
   const list = userChatsList;
 
@@ -52,7 +53,7 @@ export default function Chat() {
   return (
     <div className='h-screen flex justify-center items-center flex-row py-2'>
       <div className='h-screen w-full sm:hidden flex justify-start items-center flex-col px-2'>
-        <div className='themeCard flex justify-between items-center flex-row w-full m-2 px-3 rounded-md'>
+        {mobileActiveChat ? <></> : <div className='themeCard flex justify-between items-center flex-row w-full m-2 px-3 rounded-md'>
           <div className='flex justify-center items-center flex-row py-3'>
             <img className='w-10 me-2' src={Logo} alt="logo" />
             <h1 className='text-lg font-medium'>JellyFish</h1>
@@ -62,26 +63,63 @@ export default function Chat() {
             <span style={{ backgroundColor: "black", width: "24px", height: "3px", borderRadius: "2px" }} className='mb-1'></span>
             <span style={{ backgroundColor: "black", width: "24px", height: "3px", borderRadius: "2px" }} className='mb-1'></span>
           </nav>
-        </div>
+        </div>}
 
         <p>mobile screen still under development it may not work properly</p>
 
-        <div className='w-full flex flex-col items-center'>
-          <div className='w-full flex justify-center pb-2'>
-            <Button extraClassNames="w-full m-0" name="Add New User" onClick={() => findNewUser ? setFindNewUser(false) : setFindNewUser(true)} />
+        {mobileActiveChat ?
+          <div className='themeCard h-full sm:hidden flex flex-col items-center rounded-md w-screen'>
+            <div className='w-full flex justify-center items-center flex-row px-4'>
+              <div className='w-80'>
+                <p onClick={()=> mobileActiveChat ? setMobileActiveChat(false):setMobileActiveChat(true)}>back</p>
+              </div>
+              <h1 className='text-left w-100 text-lg font-medium py-3'>{activeChatUserChats?.userData?.user.name}</h1>
+            </div>
+            <div className='flex justify-center items-center w-full' style={{ height: "80%" }}>
+              <div className='themeCard h-full rounded-md flex justify-center items-center' style={{ width: "95%" }}>
+                {/* <img className='m-auto' src={ChatSectionBg} alt="img" /> */}
+                <div style={{ overflowY: "scroll" }} className='w-full h-full p-3'>
+                  {activeChatUserChats?.userChats?.map((chat, index) =>
+                    <div key={index}
+                      className={activeChatUserChats?.userData?.user._id === chat?.senderId ?
+                        'w-full flex flex-row justify-start items-end' : 'w-full flex flex-row justify-end items-end'}>
+                      <p
+                        className={activeChatUserChats?.userData?.user._id === chat?.senderId ?
+                          'themeCard w-fit px-2 py-1 rounded-md rounded-bl-none mb-2' : 'themeCard w-fit px-2 py-1 rounded-md rounded-br-none mb-2'}
+                      >{chat.text}</p>
+                      <p className='ps-1 pb-2 m-0' style={{ fontSize: "9px" }}>{convertToIST(chat.createdAt)}</p>
+                    </div>)
+                  }
+                  {(Array.isArray(activeChatUserChats?.userChats) && activeChatUserChats?.userChats.length === 0) ?
+                    <div className='w-full h-full flex justify-center items-center'>
+                      <p className='text-center'>Chat Session is Empty <br /> Start Conversation</p>
+                    </div>
+                    : <></>
+                  }
+                </div>
+              </div>
+            </div>
+            <div className='flex justify-center items-center flex-row mt-2' style={{ width: "95%" }}>
+              <Input value={textMessage} onChange={(e) => setTextMessage(e.target.value)} name="Message" type="Text" />
+              <Button extraClassNames="ms-2" onClick={() => sendMessage(textMessage)} name="Send" />
+            </div>
           </div>
-          {list?.map((chatUser, index) =>
-            <ChatCard onClick={() => getFullChatMessages(index)}
-              userId={chatUser.user._id} name={chatUser.user.name} key={chatUser.user._id}
-              lastMessage={user?._id === chatUser.lastMessage.senderId
-                ? `You: ${chatUser.lastMessage.text}`
-                : chatUser.lastMessage.text === ""
-                  ? ""
-                  : `${chatUser.user.name}: ${chatUser.lastMessage.text}`
-              }
-              extraClassNames="mb-2 w-full" />
-          )}
-        </div>
+          : <div className='w-full flex flex-col items-center'>
+            <div className='w-full flex justify-center pb-2'>
+              <Button extraClassNames="w-full m-0" name="Add New User" onClick={() => findNewUser ? setFindNewUser(false) : setFindNewUser(true)} />
+            </div>
+            {list?.map((chatUser, index) =>
+              <ChatCard onClick={() => {getFullChatMessages(index);setMobileActiveChat(true)}}
+                userId={chatUser.user._id} name={chatUser.user.name} key={chatUser.user._id}
+                lastMessage={user?._id === chatUser.lastMessage.senderId
+                  ? `You: ${chatUser.lastMessage.text}`
+                  : chatUser.lastMessage.text === ""
+                    ? ""
+                    : `${chatUser.user.name}: ${chatUser.lastMessage.text}`
+                }
+                extraClassNames="mb-2 w-full" />
+            )}
+          </div>}
       </div>
 
       <div className='themeCard sm:flex hidden justify-between items-center flex-col h-full m-2 rounded-md' style={{ width: "25%" }}>
